2bd0d2705169cc089d9599f65f7cd42ed8a92c36
UnitedCatdom <78739707+CuteBladeYT@users.noreply.github.com>
2022-01-05 19:10:30 +0100

    Update v1.1
      Name: Better Selector
      Author: UnitedCatdom
      Version: v1.1
    
    
    
      Usage: qs(object, selector)
      object: HTML Element's CSS Path. i.e. ".class #id element.withClass#andId"
      selector: normally you would use document.querySelectorAll("element")[4], for example,
                now you just need to type any int > 0 (higher than zero), or -1 to disable.
                (document.querySelectorAll("element"))

--- betterSelector.js
+++ betterSelector.js
@@ -1,11 +1,31 @@
-function qs(object) {
-  return document.querySelector(object);
+function qs(obj, sel) {
+  if (sel) {
+    if (sel == -1) {
+      return document.querySelectorAll(obj);
+    } else {
+      return document.querySelectorAll(obj)[sel];
+    }
+  } else {
+    return document.querySelector(obj);
+  }
 }
 
-function qsA(object, selector) {
-  if (selector) {
-    return document.querySelectorAll(object)[selector];
-  } else {
-    return document.querySelectorAll(object);
-  };
-}
+
+
+
+/*
+
+  Name:     Better Selector
+  Author:   UnitedCatdom
+  Version:  v1.1
+  Github:   https://github.com/CuteBladeYT/JavaScripts/edit/better-js/betterSelector.js
+
+
+
+  Usage: qs(object, selector)
+  object:   HTML Element's CSS Path. i.e. ".class #id element.withClass#andId"
+  selector: normally you would use document.querySelectorAll("element")[4], for example,
+            now you just need to type any int > 0 (higher than zero), or -1 to disable.
+            (document.querySelectorAll("element"))
+
+*/
